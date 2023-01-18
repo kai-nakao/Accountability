62a0a6439ab13bd06f3e69014e98b499b604a1b4
kai3nakao <kai.nakao@scopenext.jp>
2023-01-18 10:23:13 +0900

    create app

--- test/Contract.test.js
+++ test/Contract.test.js
@@ -36,7 +36,7 @@ describe('Contract.sol', function () {
     // Fixtures can return anything you consider useful for your tests
     return { accountability, accountabilityNfts, owner, addr1, addr2 }
   }
-  // Should be able to deposit funds into the contract using the lockFunds function
+
   it('Should be able to deposit funds into the contract using the lockFunds function', async function () {
     const { accountability, owner, addr1 } = await loadFixture(
       deployContractFixture,
@@ -52,7 +52,69 @@ describe('Contract.sol', function () {
     expect(lockedFunds.lockedAt).to.equal(await time.latest())
   })
 
-  // Should reject a withdrawal if the user does not have an NFT from the contract
-  // Should reject a withdrawal if the user has 0 funds
-  // Maybe let's try replicate some kind of reentrancy attack
+  it('Should be able to withdraw funds from the contract using the withdrawFunds function', async function () {
+    const { accountability, owner, addr1 } = await loadFixture(
+      deployContractFixture,
+    )
+    await accountability.lockFunds(time.duration.minutes(1), {
+      value: ethers.BigNumber.from(1000),
+    })
+    lockedFunds = await accountability.lockedFunds(owner.address)
+
+    // Try and withdraw before the time has passed
+    await expect(accountability.connect(owner).withdraw()).to.be.revertedWith(
+      "You can't withdraw yet.",
+    )
+  })
+
+  it("Should reject a withdrawal if the user doen't have an NFT from the NFT contract", async function () {
+    const { accountability, owner, addr1 } = await loadFixture(
+      deployContractFixture,
+    )
+    await accountability.lockFunds(time.duration.minutes(1), {
+      value: ethers.BigNumber.from(1000),
+    })
+    lockedFunds = await accountability.lockedFunds(owner.address)
+
+    // Wait 1 minute
+    await time.increase(time.duration.minutes(1))
+
+    // Fail to withdraw does not have an NFT
+    await expect(accountability.connect(owner).withdraw()).to.be.revertedWith(
+      'You do not own an NFT from the other smart contract.',
+    )
+  })
+
+  it('Should reject a withdrawal if the user has 0 funds', async function () {
+    const { accountability, owner, addr1 } = await loadFixture(
+      deployContractFixture,
+    )
+    await expect(accountability.connect(owner).withdraw()).to.be.revertedWith(
+      'You have no locked funds.',
+    )
+  })
+
+  it('Successfully withdraw funds if the time has passed and the user has an NFT', async function () {
+    const {
+      accountability,
+      accountabilityNfts,
+      owner,
+      addr1,
+    } = await loadFixture(deployContractFixture)
+    await accountability.lockFunds(time.duration.minutes(1), {
+      value: ethers.BigNumber.from(1000),
+    })
+
+    // Wait 1 minute
+    await time.increase(time.duration.minutes(1))
+
+    // we need to mint an NFT for the owner
+    await accountabilityNfts.mintTo(owner.address, 'url')
+
+    await accountability.connect(owner).withdraw()
+
+    // Expect the lockedFunds mapping to be empty
+    const lockedFunds = await accountability.lockedFunds(owner.address)
+    expect(lockedFunds.amount).to.equal(ethers.BigNumber.from(0)) // 0
+  })
 })
