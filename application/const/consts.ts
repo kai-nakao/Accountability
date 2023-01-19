5a949977ced8e016cede0897ae3a6dcbdd65828f
kai3nakao <kai.nakao@scopenext.jp>
2023-01-19 10:36:39 +0900

    connect discord

--- application/const/consts.ts
+++ application/const/consts.ts
@@ -1,4 +1,10 @@
+import { ChainId } from "@thirdweb-dev/sdk"
+
 export const ACCOUNTABILITY_CONTRACT_ADDRESS =
   '0x2377155fc7A5e065E4EABcAdCe1A82403345d827'
 export const NFT_CONTRACT_ADDRESS = '0x1611759FdfE7908861F2e254D4A61f320A3A7B34'
 
+export const CHAIN = ChainId.Goerli
+
+
+export const DISCORD_SERVER_ID = '1065425745909923870'
